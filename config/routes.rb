e1e3f0f50438c4b880668834ef0a119246de8d73
rokhimin <rokhim.whd@gmail.com>
2019-11-09 11:42:58 +0000

    SUCC MY ASS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'index#home'
-  get '/json-to-hash-ruby', to: 'jsonhash#app'
-	post '/json-to-hash-ruby', to: 'jsonhash#parsing'
+  get '/json-to-hash-ruby.rb', to: 'jsonhash#app'
+	post '/json-to-hash-ruby.rb', to: 'jsonhash#parsing'
 end
